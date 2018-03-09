fa21d57bb4db4cb0c39771f429adcbaafb6e13ee
wenboHuang <59469024@qq.com>
2018-03-09 17:10:41 +0800

    1

---
+++ lesson2/src/main/java/org/lesson2/App.java
@@ -0,0 +1,16 @@
+package org.lesson2;
+
+/**
+ * 
+ * @author huangwenbo
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+}
