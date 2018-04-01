980db22ea5092c5b5b0528d507eef82f537cac30
wenboHuang <59469024@qq.com>
2018-04-01 17:49:30 +0800

    1

--- redis-demo/src/main/java/org/mysql/service/UserServiceImp.java
+++ redis-demo/src/main/java/org/mysql/service/UserServiceImp.java
@@ -10,25 +10,21 @@ public class UserServiceImp implements UserService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Override
     public void createUser(String name, Integer age) {
         // TODO Auto-generated method stub
         jdbcTemplate.update("insert into USERS(NAME, AGE) values(?, ?)", name, age);
     }
 
-    @Override
     public void deleteByName(String name) {
         // TODO Auto-generated method stub
         jdbcTemplate.update("delete from USERS where NAME = ?", name);
     }
 
-    @Override
     public Integer getAllUsers() {
         // TODO Auto-generated method stub
         return jdbcTemplate.queryForObject("select count(1) from USERS", Integer.class);
     }
 
-    @Override
     public void deleteAllUsers() {
         // TODO Auto-generated method stub
         jdbcTemplate.update("delete from USERS");
--- redis-demo/src/main/java/org/redis/demo/App.java
+++ redis-demo/src/main/java/org/redis/demo/App.java
@@ -1,5 +1,8 @@
 package org.redis.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.Jedis;
 
 /**
@@ -8,17 +11,21 @@ import redis.clients.jedis.Jedis;
  */
 public class App 
 {
-//    public static void main( String[] args )
-//    {
-//        //System.out.println( "Hello World!" );
-//        Jedis jedis = new Jedis("47.95.3.38");
-//        System.out.println("连接成功");
-//        //查看服务是否运行
-//        System.out.println("服务正在运行: "+jedis.ping());
-////        jedis.set("hostName", "ubuntuAliyun");
-////        System.out.println(jedis.get("hostName"));
-//        jedis.del("hostName");
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+    
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        Jedis jedis = new Jedis("47.95.3.38");
+        jedis.auth("redis");
+        logger.info("连接成功");
+        //查看服务是否运行
+        logger.warn("服务正在运行:{}",jedis.ping());
+//        jedis.set("hostName", "ubuntuAliyun");
 //        System.out.println(jedis.get("hostName"));
-//        jedis.close();
-//    }
+        jedis.del("hostName");
+        System.out.println();
+        logger.info(jedis.get("hostName"));
+        jedis.close();
+    }
 }
